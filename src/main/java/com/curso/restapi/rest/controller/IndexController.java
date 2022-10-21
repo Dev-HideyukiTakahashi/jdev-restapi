76073d0ec7375ced503e3d05c0f38efe05e08b83
Hideyuki Takahashi <yuki.murasaki90@gmail.com>
2022-10-21 15:53:51 -0300

    Aula - Post

--- src/main/java/com/curso/restapi/rest/controller/IndexController.java
+++ src/main/java/com/curso/restapi/rest/controller/IndexController.java
@@ -3,11 +3,14 @@ package com.curso.restapi.rest.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,5 +57,11 @@ public class IndexController {
   public ResponseEntity<List<Usuario>> consultaTodos() {
     List<Usuario> usuarios = usuarioRepository.findAll();
     return ResponseEntity.ok(usuarios);
+  }
+
+  @PostMapping(path = "/")
+  public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
+    Usuario usuarioSalvo = usuarioRepository.save(usuario);
+    return ResponseEntity.ok(usuarioSalvo);
   }
 }
